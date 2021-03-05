c8b3c278b959f75977858b6ba0cfaa11f10e8d0c
SaliMonium <sms8.net@gmail.com>
2021-03-05 20:59:59 +0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>Open Source</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
@@ -18,7 +18,7 @@ const features = [
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>Security Tips</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
@@ -28,7 +28,7 @@ const features = [
     ),
   },
   {
-    title: <>Powered by React</>,
+    title: <>recommendations</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
